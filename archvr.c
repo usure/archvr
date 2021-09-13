069c2788d5e7f3f9e0496657ac1ec6c6a0eaea6b
graysz <usure@users.noreply.github.com>
2021-09-13 12:04:13 -0400

    Update archvr.c

--- archvr.c
+++ archvr.c
@@ -5,29 +5,47 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+
+int start()
 {
-	char input[100]; /* set input variable [char]*/
-	int i = 0;
-	char delim[] ="/"; /* "/" marks the end of a specific chunk of data */
 	printf("| - ASCM Downloader for archive.org 1 Hour Books - |\n");
 	printf("--- Before using this program...--- ");
 	printf("\n1. Find the book you want to download on archive.org.\n");
 	printf("2. Borrow it.\n");
 	printf("3. Copy the URL. (ex: https://archive.org/details/[bookname] <-- it must look exactly like this)\n");
+	return 0;
+}
+
+int core()
+{
 	printf("\nEnter link: \n"); /* asks user for input */
-	gets(input); /* gets input from user, stores it in var */
-	char *ar[100]; /* sets array with pointer */	
+	//vars
+		char input[100]; /* set input variable [char]*/
+		int i = 0;
+		char delim[] ="/"; /* "/" marks the end of a specific chunk of data */
+		gets(input); /* gets input from user, stores it in var */
+		char *ar[100]; /* sets array with pointer */	
 
 	/* sets token variable pointer */
-	char *token = strtok (input, delim);
+		char *token = strtok (input, delim);
 
 	/* while the token is non-empty, put token until NULL */
-	 while(token !=NULL) {
-		token = strtok(NULL, delim);
-		ar[i++] = token; /*it takes each section and stores it in an array */
+
+		 while(token !=NULL) {
+			token = strtok(NULL, delim);
+			ar[i++] = token; /*it takes each section and stores it in an array */
+		/*puts(token);*/
 	}
 	printf("\n----------------------\n");
-	printf("https://archive.org/services/loans/loan/?action=media_url&identifier=%s&format=pdf&redirect=1", ar[2]);
-  return 0;
+	printf("https://archive.org/services/loans/loan/?action=media_url&identifier=%s&format=pdf&redirect=1\n", ar[2]);
+	/* printf("%s\n", ar[2]); */ 
+	return 0;
+}
+
+int main()
+{
+	
+	start();
+	core();
+   	return 0;
 }
